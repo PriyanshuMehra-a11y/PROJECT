c53f8d7ecf8c53e256ba43fb7b2a6ca8aae7e137
PriyanshuMehra-a11y <mehramillie4@gmail.com>
2025-12-04 09:59:15 +0530

    Clock.h

--- include/Clock.h
+++ include/Clock.h
@@ -1,12 +1,24 @@
-#ifndef DIGITAL_CLOCK_H
-#define DIGITAL_CLOCK_H
-
 #include <stdio.h>
 #include <time.h>
-#include <unistd.h>   
-#include <stdlib.h>  
-// Function Prototypes
-void displayClock();   
-void runClock();       
-
+#include <string.h>
+#include <ctype.h>
+#ifdef _WIN32
+    #include <windows.h>
+    #define CLEAR() system("cls")
+    #define PAUSE_1S() Sleep(1000)
+    #define BEEP() Beep(1000, 300)
+#else
+    #include <unistd.h>
+    #define CLEAR() system("clear")
+    #define PAUSE_1S() sleep(1)
+    #define BEEP() printf("\a")
 #endif
+
+// Struct for tasks
+typedef struct {
+    char name[100];
+    int hour24;
+    int minute;
+    int reminded;
+} Task;
+
